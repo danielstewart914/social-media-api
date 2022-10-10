630c9b34cfb860a724bb49b5ed8cfcd8bdabe5c3
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-10-10 12:52:17 -0700

    Added seed data generation

---
+++ utils/seed.js
@@ -0,0 +1,80 @@
+const connection = require( '../config/connection' );
+const { User, Thought } = require( '../models' );
+const userData = require( './users.json' );
+const words = require( './words.json' );
+
+connection.on( 'error', (err) => err );
+
+const seedFriends = async ( users ) => {
+    let friend;
+
+    for ( const i in users ) {
+        const friendsArray = [];
+        for ( let j = 0; j < Math.floor( Math.random() * 6 ); j++ ) {
+            do {
+                friend = Math.floor( Math.random() * users.length );
+            } while ( friend == i );
+            friendsArray.push(users[friend]._id);
+        }
+        await User.updateOne( { _id: users[i]._id }, {
+            $push: { friends: { $each: friendsArray } }
+        } )
+    }
+}
+
+const randomIndex = array => Math.floor( Math.random() * array.length );
+const getRandomWord = () => words[randomIndex(words)];
+
+const genRandomSentence = ( min, max ) => {
+    let thought = '';
+    // create random post length between min and max
+    const postLength = Math.random() * ( max - min ) + min;
+    for ( let i = 0; i < postLength; i++) {
+        if ( i !== 0 ) thought += ` ${ getRandomWord() }`;
+        else thought += `${ getRandomWord() }`;
+    }
+    return thought;
+}
+
+const seedThoughts = async ( users ) => {
+    for ( const user of users ) {
+        const thoughtsArray = [];
+        for ( let i = 0; i < Math.floor( Math.random() * 6 ); i++ ) {
+            const reactionArray = [];
+            for ( let j = 0; j < Math.floor( Math.random() * 5 ); j++ ) {
+                reactionArray.push ( {
+                    reactionBody: genRandomSentence( 3, 5 ),
+                    username: users[randomIndex( users )].username
+                } );
+            }
+            const thought = {
+                thoughtText: genRandomSentence( 10, 20 ),
+                username: user.username,
+                reactions: reactionArray
+            }
+            const newThought = await Thought.create( thought );
+            thoughtsArray.push( newThought._id );
+        }
+        await User.updateOne( 
+            { _id: user._id }, { 
+            $push: { thoughts: { $each: thoughtsArray } } 
+        } );
+    }
+}
+
+connection.once( 'open', async () => {
+    console.log( 'connected' );
+
+    await User.deleteMany();
+    await Thought.deleteMany();
+
+    const users = await User.insertMany( userData );
+    console.log( '===== USERS CREATED =====' );
+    await seedFriends( users );
+    console.log( '===== FRIENDS SEEDED =====' );
+    await seedThoughts( users );
+    console.log( '===== THOUGHTS AND REACTIONS SEEDED =====' );
+
+    console.info( 'Seeding complete!' );
+    process.exit(0);
+} );
