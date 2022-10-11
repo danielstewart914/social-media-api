67ec7fc2384fbf389fba4d5c740e885509208625
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-10-10 17:43:14 -0700

    Removed timezone from date time formatting

--- utils/formatDateTime.js
+++ utils/formatDateTime.js
@@ -1,6 +1,6 @@
 const formatDateTime = timestamp => {
     const date = new Date( timestamp );
-    return date.toLocaleDateString( 'en-us', { year: 'numeric', month: 'short', day: 'numeric' } ) + ' at ' + date.toLocaleTimeString( 'en-us', { hour: 'numeric', minute: 'numeric', timeZoneName: 'short' } );
+    return date.toLocaleDateString( 'en-us', { year: 'numeric', month: 'short', day: 'numeric' } ) + ' at ' + date.toLocaleTimeString( 'en-us', { hour: 'numeric', minute: 'numeric' } );
 }
 
 module.exports = formatDateTime;
